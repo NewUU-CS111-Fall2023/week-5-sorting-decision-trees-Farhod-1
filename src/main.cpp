1a673654f28100a1da1b19d7f0650eefd2ac09d3
Farxod Qarshiyev <f.qarshiyev@newuu.uz>
2023-11-16 19:19:06 +0500

    problems solved

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 int main() {
     std::cout << "Task 1" << std::endl;
     // call for task 1
-//
+
     std::cout << "Task 2" << std::endl;
     // call for task 2
     std::cout << "Task 3" << std::endl;
